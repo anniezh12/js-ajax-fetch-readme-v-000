40d8f98a21225b30d6e912833a8b04d8aeaca8bc
¨anniezh12¨ <aniqaelahi@yahoo.com>
2017-12-15 20:41:16 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+const token = 'Token'
+$(document).ready(function(){
+  fetch('https://api.github.com/user/repos',{
+    header: {  Authorization: 'token ${token}'
+  }
+  }).then(res => res.json()).then(json => console.log(json));
+});
